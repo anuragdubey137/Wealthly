bece89c941beab908da90a1128835f5fa0729913
anuragdubey137 <anuragdubey13704@gmail.com>
2026-01-13 23:22:55 +0530

    add post request

--- next-app/app/api/transaction/route.ts
+++ next-app/app/api/transaction/route.ts
@@ -1,8 +1,39 @@
+
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest } from "next/server";
 
+const prisma = new PrismaClient();
 
 export async function POST(req:NextRequest) {
-    return new Response('Transaction created successfully');
+    const session = await getServerSession();
+    if(!session || !session.user){
+
+        return new Response('Unauthorized', { status: 401 });
+    }
+    const body = await req.json();
+    // validate input here
+
+    if (!session.user.email) {
+        return new Response('Unauthorized', { status: 401 });
+    }
+    
+    const transaction = await prisma.transaction.create({
+        data: {
+            amount: body.amount,
+            type: body.type,
+            category: body.category,
+            description: body.description ?? "",
+            user: {
+                connect: { email: session.user.email }
+            },
+            date: new Date(body.date)
+        }
+    });
+    return new Response(JSON.stringify(transaction), { status: 201 });
+
+
+
 }
 export async function GET(req:NextRequest) {
     return new Response('Transaction API is working');
